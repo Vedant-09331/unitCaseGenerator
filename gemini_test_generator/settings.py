49ecd5e3dfe7c525bca4156c7adee98f0b85bfa8
Vedant-09331 <vedantrana0930@gmail.com>
2025-07-16 16:16:13 +0530

    Update settings.py

--- gemini_test_generator/settings.py
+++ gemini_test_generator/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-here-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['unit-test-case-generator-8rie.onrender.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['unitcasegenerator.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition
